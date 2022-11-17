62683939fee22edd6fb79c2ca63d70b5e56fa426
Antonio Carvalho <antoniorcn@hotmail.com>
2022-11-17 17:49:54 -0300

    Aula 14 POO Tarde parte 3

---
+++ poo-tarde/aula14/src/edu/curso/TesteBanco.java
@@ -0,0 +1,25 @@
+package edu.curso;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteBanco {
+	public static final String URL = "jdbc:mariadb://localhost:3306/petshop";
+	public static final String USER = "root";
+	public static final String PASSWORD = "123456";
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			System.out.println("Classe Carregada");
+			
+			Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Conectado ao banco de dados");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
